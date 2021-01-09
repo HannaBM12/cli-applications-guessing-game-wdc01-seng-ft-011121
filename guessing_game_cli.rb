cb0ad1bf45a712a1fc6e44346c564cb73d61758c
Hanna Mulugeta <hannabmulugeta@gmail.com>
2021-01-09 22:56:45 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
   # puts "Guess a number between 1 and 6"
   input = gets
   input = input.chomp
-
+  binding.pry
   if input == 'exit'
     puts "Goodbye!"
   elsif (input).to_i == number
