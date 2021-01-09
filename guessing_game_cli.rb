b3f73f711077e0720bc55f995080c450d405f37c
Hanna Mulugeta <hannabmulugeta@gmail.com>
2021-01-09 22:55:39 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 def run_guessing_game
   number = rand(7)
-  puts "Guess a number between 1 and 6"
+  # puts "Guess a number between 1 and 6"
   input = gets
   input = input.chomp
 
