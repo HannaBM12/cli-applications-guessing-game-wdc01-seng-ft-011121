77a4325e4325cfc90e03a1d97b25a5e946b66ce1
Hanna Mulugeta <hannabmulugeta@gmail.com>
2021-01-09 22:46:43 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,6 +4,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6"
   input = gets
   input = input.chomp.to_i
+  
   case  input
   when number
     puts "You guessed the correct number!"
